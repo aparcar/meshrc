59a59d4677550979d79204764585154c199a7d08
Paul Spooren <spooren@informatik.uni-leipzig.de>
2018-02-18 21:56:28 +0100

    fix bmx scraper scripts

--- prometheus_bmx6_targets.sh
+++ prometheus_bmx6_targets.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-# adds all bmx7 originators to hosts directory
+# adds all bmx6 originators to hosts directory
 bmx6 -c originators | tail -n +3 | awk '{ print $3" "$1 }' >> /tmp/hosts/bmx6_originators
 
 # make dnsmasq reread the host file
 /etc/init.d/dnsmasq reload
 
-# create new prometheus targets file
-echo "- targets:" > /tmp/targets.yml.tmp
-cat /tmp/hosts/bmx6_originators | awk '{ print "  - "$2":9100" }' | sort >> /tmp/targets_bmx6.yml.tmp
-
-# only overwrite targets if it actually change
-if [[ "$(sha256sum /var/targets_bmx6.yml.tmp | cut -c -32)" != "$(sha256sum /var/targets_bmx6.yml | cut -c -32)" ]]; then
-        # this is done to not lose any offline nodes
-        cat /var/targets_bmx6.yml >> /var/targets_bmx6.yml.tmp
-        cat /var/targets_bmx6.yml.tmp | sort | uniq > /var/targets_bmx6.yml
-        cp /var/targets_bmx6.yml.tmp /var/targets_bmx6.yml
-        echo "updated"
-fi
+# create new prometheus targets_bmx6 file
+cat /tmp/hosts/bmx6_originators | awk '{ print "  - "$2":9100" }' > /tmp/targets_bmx6.yml.tmp
+cat /var/targets_bmx6.yml | tail -n +2 >> /var/targets_bmx6.yml.tmp
+echo "- targets:" > /tmp/targets_bmx6.yml
+cat /var/targets_bmx6.yml.tmp | sort | uniq >> /var/targets_bmx6.yml
+cp /var/targets_bmx6.yml /var/targets_bmx6.yml.tmp

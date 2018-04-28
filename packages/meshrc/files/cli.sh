41bc14e0fd63668e5bc97a85580c6948cba76df0
Paul Spooren <spooren@informatik.uni-leipzig.de>
2018-04-28 19:56:11 +0900

    add usage function
    remove lime-defaults sync, this should be implemented in a better way

--- packages/meshrc/files/cli.sh
+++ packages/meshrc/files/cli.sh
@@ -25,25 +25,9 @@ bmx7_apply_changes() {
     /etc/init.d/bmx7 reload
 }
 
-sync_defaults() {
-    case $1 in
-        true|1)
-            echo "sync lime-defauls with the cloud"
-            bmx7_add_sms_file "/etc/config/lime-defaults"
-        ;;
-        false|0)
-            echo "stop sync of lime-defauls"
-            bmx7_del_sms_file "/etc/config/lime-defaults"
-        ;;
-        *)
-            echo "unknown option for -l|--lime-defaults"
-        ;;
-    esac
-}
-
 reset_network() {
     read -p "confirm network reset [y/N]" -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]]
+    if [[ $REPLY == "Y" || $REPLY == "y" ]]
     then
         echo "send reset command to mesh network"
         touch /var/run/bmx7/sms/sendSms/reset
@@ -51,53 +35,66 @@ reset_network() {
     else
         echo "reset canceled"
     fi
-
 }
 
-set_node_hostname() {
+set_node_name() {
     echo "set node shortId ${1} to ${2}"
     echo "$2" > "/var/run/bmx7/sms/sendSms/hn_${1}"
     bmx7_add_sms_entry "hn_${1}"
 }
 
-set_node_ap_psk() {
+set_node_ap_password() {
     echo "set access point password for shortId ${1}"
     echo "$2" > "/var/run/bmx7/sms/sendSms/ap_${1}"
     bmx7_add_sms_entry "ap_${1}"
 }
 
-set_ap_psk() {
+set_ap_password() {
     echo "set access point password for all nodes"
     echo "$1" > "/var/run/bmx7/sms/sendSms/ap"
     bmx7_add_sms_entry "ap"
 }
 
-set_mesh_psk() {
+set_mesh_password() {
     echo "set mesh password for all nodes"
     echo "$1" > "/var/run/bmx7/sms/sendSms/mesh"
     bmx7_add_sms_entry "mesh"
 }
 
+usage() {
+    cat << EOF
+Usage: $0 
+
+    -h --help                           : show this message
+    -l --list-nodes                     : show all nodes of mesh network
+    -n --node-name <shortId> <name>     : sets node name for given shortId
+    -a --ap-pass <pass>                 : set access point password of all nodes
+    -m --mesh-pass <pass>               : set mesh password of all nodes
+    -p --node-ap-pass <shortId> <pass>  : set access point password of shortId
+    -r --reset                          : resets all nodes by removing overlayfs
+EOF
+}
+
 while [ "$#" ]; do
     case $1 in
-        -d=*|--lime-defaults=*)
-            sync_defaults "{i#*=}"
+        -h|--help)
+            usage
             shift
             ;;
-        -h|--hostname)
-            set_node_hostname "$2" "$3"
+        -n|--node-name)
+            set_node_name "$2" "$3"
             shift; shift; shift
             ;;
-        -p|--node_ap_psk)
-            set_node_ap_psk "$2" "$3"
+        -p|--node-ap-pass)
+            set_node_ap_password "$2" "$3"
             shift; shift; shift
             ;;
-        -a|--ap_psk)
-            set_ap_psk "$2"
+        -a|--ap-pass)
+            set_ap_password "$2"
             shift; shift
             ;;
-        -m|--mesh_psk)
-            set_mesh_psk "$2"
+        -m|--mesh-pass)
+            set_mesh_password "$2"
             shift; shift
             ;;
         -l|--list-nodes)
@@ -106,6 +103,7 @@ while [ "$#" ]; do
             ;;
         -r|--reset)
             reset_network
+            shift
             ;;
         *)
             break

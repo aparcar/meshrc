87e4f3ba9aa74f3de4e1f2ceeada0711d1dcd362
Paul Spooren <spooren@informatik.uni-leipzig.de>
2018-04-30 23:18:47 +0900

    format graph ifnro as well
    Signed-off-by: Paul Spooren <spooren@informatik.uni-leipzig.de>

---
+++ meshrc/static/properties.js
@@ -0,0 +1,7 @@
+properties_active = {
+    "load": ["Load", false],
+    "memory": ["Free memory", fmt_percent],
+    "traffic_mesh": ["Traffic Mesh", fmt_filesize],
+    "traffic_ap": ["Traffic AP", fmt_filesize],
+    "uptime": ["Uptime", fmt_duration]
+}
